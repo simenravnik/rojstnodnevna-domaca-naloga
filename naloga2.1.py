d4043fea858ac32da2bc58db7666f74dc3828aa7
tinkaresek <resek.tinka@gmail.com>
2019-03-27 13:41:53 +0100

    Reseni naloga2.1.py in naloga2.2.py

--- naloga2.1.py
+++ naloga2.1.py
@@ -1,1 +1,11 @@
 #preveri ali je stevilo prastevilo
+
+n = int(input("Vpisite pozitivno celo stevilo: "))
+
+for i in range (2,n):
+    if n % i == 0:
+        print("Stevilo " + str(n) + " ni prastevilo.")
+        break
+
+if i == n-1:
+    print("Stevilo " + str(n) + " je prastevilo.")
--- naloga2.2.py
+++ naloga2.2.py
@@ -1,1 +1,12 @@
 #izpise vsa prastevila od 1 - 100
+
+i = 2
+
+for n in range (1,101):
+    for i in range (2,n):
+        if n % i == 0:
+            #print("Stevilo " + str(n) + " ni prastevilo.")
+            break
+
+    if i == n-1:
+        print(n)
