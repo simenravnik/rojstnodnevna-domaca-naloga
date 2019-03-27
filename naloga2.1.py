e06f84ab59b6bd5c7c2d6526a3dbd876dd3e7bdd
tinkaresek <resek.tinka@gmail.com>
2019-03-27 16:50:05 +0100

    Popravek naloga2.1 in naloga2.2

--- naloga2.1.py
+++ naloga2.1.py
@@ -2,6 +2,7 @@
 
 n = int(input("Vpisite pozitivno celo stevilo: "))
 
+i = 1
 for i in range (2,n):
     if n % i == 0:
         print("Stevilo " + str(n) + " ni prastevilo.")
@@ -9,3 +10,5 @@ for i in range (2,n):
 
 if i == n-1:
     print("Stevilo " + str(n) + " je prastevilo.")
+else:
+    print("Stevilo " + str(n) + " ni prastevilo.")
--- naloga2.2.py
+++ naloga2.2.py
@@ -1,6 +1,6 @@
 #izpise vsa prastevila od 1 - 100
 
-i = 2
+i = 1
 
 for n in range (1,101):
     for i in range (2,n):
